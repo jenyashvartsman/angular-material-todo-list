6f5fd07926bb73d2accc05c4acb5eb9aeab50507
Jenya Shvartsman <jenyashvartsman@gmail.com>
2022-10-25 21:28:11 -0400

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderModule } from './components/header/header.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    HeaderModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
